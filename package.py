3563fd5395c5f5d24c0d905610b21024f12d1abd
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2025-01-20 11:17:32 -0500

    Rez initial files

---
+++ package.py
@@ -0,0 +1,31 @@
+name = 'studiolibrary'
+
+version = '2.14.1.hh.1.0.0'
+
+authors = [
+    'Kurt Rathjen',
+]
+
+description = '''Animation pose library'''
+
+with scope('config') as c:
+    import os
+    c.release_packages_path = os.environ['HH_REZ_REPO_RELEASE_EXT']
+
+requires = [
+]
+
+private_build_requires = [
+]
+
+variants = [
+]
+
+def commands():
+    env.REZ_STUDIOLIBRARY_ROOT = '{root}'
+    env.PATH.append('{root}/bin')
+    env.PYTHONPATH.append('{root}/src/python')
+
+
+build_command = 'rez python {root}/rez_build.py'
+uuid = 'repository.studiolibrary'
